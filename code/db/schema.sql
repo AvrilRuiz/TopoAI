f9284e97c5b0691c521db72c634f8af7aa273430
Avril <avril03michruiz06@gmail.com>
2024-09-11 15:11:43 -0600

    feat: added new table to sql database

--- code/db/schema.sql
+++ code/db/schema.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS vegetation_index (
   longitude FLOAT(13) NOT NULL,
   latitude FLOAT(13) NOT NULL
 )
+
+CREATE TABLE IF NOT EXISTS data_for_models (
+  point_id SMALLINT NOT NULL,
+  date DATE NOT NULL,eed
+  ndvi FLOAT(13) NOT NULL,
+  ndwi FLOAT(13) NOT NULL,
+  longitude FLOAT(13) NOT NULL,
+  latitude FLOAT(13) NOT NULL,
+  temperature FLOAT(13) NOT NULL,
+  soil_type VARCHAR(255) NOT NULL,
+  season VARCHAR(255) NOT NULL,
+  holiday VARCHAR(255) NOT NULL,
+  altutude FLOAT(13) NOT NULL
+);
